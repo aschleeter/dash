6d7dbfc4c25978996ec2b8a9cb8725646233ec02
aschleeter <44715235+aschleeter@users.noreply.github.com>
2019-05-12 01:23:22 -0400

    GET test for zac change localhost add DASHdashboard

--- src/app/zac/test.service.ts
+++ src/app/zac/test.service.ts
@@ -11,6 +11,6 @@ export class TestService {
   constructor(public http: HttpClient) { }
 
   public getTest(): Observable<any> {
-     return this.http.get('http://localhost:30056');
+     return this.http.get('http://localhost:30056/DASHDashboard');
   }  
 }
