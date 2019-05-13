d691143d5e90a35729e820bdb5ff8b96666db312
aschleeter <44715235+aschleeter@users.noreply.github.com>
2019-05-13 10:39:39 -0400

    GET test, remove urls that don't work

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -20,64 +20,7 @@ export class TestComponent implements OnInit {
 
   ngOnInit() {}
 
-  public onGet1(): Observable<Object> {
-    this.testservice.getTest1().subscribe(
-       data => {
-        console.log(data);
-      },
-      err => {
-        console.log(err);
-      }
-  );
-  return;
-  }
-
-  public onGet2(): Observable<Object> {
-    this.testservice.getTest2().subscribe(
-       data => {
-        console.log(data);
-      },
-      err => {
-        console.log(err);
-      }
-  );
-  return;
-  }
-
-  public onGet3(): Observable<Object> {
-    this.testservice.getTest3().subscribe(
-       data => {
-        console.log(data);
-      },
-      err => {
-        console.log(err);
-      }
-  );
-  return;
-  }
-  public onGet4(): Observable<Object> {
-    this.testservice.getTest4().subscribe(
-       data => {
-        console.log(data);
-      },
-      err => {
-        console.log(err);
-      }
-  );
-  return;
-  }
-  public onGet5(): Observable<Object> {
-    this.testservice.getTest5().subscribe(
-       data => {
-        console.log(data);
-      },
-      err => {
-        console.log(err);
-      }
-  );
-  return;
-  }
-    public onGet6(): Observable<Object> {
+  public onGet6(): Observable<Object> {
     this.testservice.getTest6().subscribe(
        data => {
         console.log(data);
@@ -88,44 +31,10 @@ export class TestComponent implements OnInit {
   );
   return;
   }
-  public onGet7(): Observable<Object> {
-    this.testservice.getTest7().subscribe(
-       data => {
-        console.log(data);
-      },
-      err => {
-        console.log(err);
-      }
-  );
-  return;
-  }
-  public onGet8(): Observable<Object> {
-    this.testservice.getTest8().subscribe(
-       data => {
-        console.log(data);
-      },
-      err => {
-        console.log(err);
-      }
-  );
-  return;
-  }
-  public onGet9(): Observable<Object> {
-    this.testservice.getTest9().subscribe(
-       data => {
-        console.log(data);
-      },
-      err => {
-        console.log(err);
-      }
-  );
-  return;
-  }
+  
   public onPost(): void {
-    if (this.testForm.valid) {
-      const postData = this.testForm.value;
-
-      this.testservice.postTest(postData).subscribe(
+    const postData = this.testForm.value;
+    this.testservice.postTest(postData).subscribe(
         data => {
           console.log(data);
         },
@@ -133,6 +42,5 @@ export class TestComponent implements OnInit {
           console.log(err);
         }
       );
-    }
   }
 }
