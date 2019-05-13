9b3ae57fa6964bab862d66ab879a7907cfe44ed0
aschleeter <44715235+aschleeter@users.noreply.github.com>
2019-05-13 16:17:10 -0400

    POST test: plain text

--- src/app/zac/test.service.ts
+++ src/app/zac/test.service.ts
@@ -5,7 +5,7 @@ import { HttpHeaders } from '@angular/common/http';
 
 const httpOptions = {
   headers: new HttpHeaders({
-    'Content-Type':  'application/json'
+    'Content-Type':  'text/plain'
   })
 };
 
